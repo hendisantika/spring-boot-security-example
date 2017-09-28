5c55c44497899731155597cca31563b0801bda3b
Hendi Santika <hendisantika@gmail.com>
2017-09-28 07:18:25 +0700

    #2 - Added Main App

---
+++ src/main/java/com/hendisantika/example/Application.java
@@ -0,0 +1,31 @@
+package com.hendisantika.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/28/17
+ * Time: 5:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
