5c34a8471607c2e1ded2560fa7f481841f031939
Hendi Santika <hendisantika@gmail.com>
2017-09-29 06:43:25 +0700

    #8 - Added User Domain

---
+++ src/main/java/com/hendisantika/example/domain/User.java
@@ -0,0 +1,71 @@
+package com.hendisantika.example.domain;
+
+import javax.persistence.*;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/28/17
+ * Time: 7:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email.replaceFirst("@.*", "@***") +
+                ", passwordHash='" + passwordHash.substring(0, 10) +
+                ", role=" + role +
+                '}';
+    }
+}
