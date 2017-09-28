c56086da84b0b2e203421996734a4df0b055e763
Hendi Santika <hendisantika@gmail.com>
2017-09-29 06:43:45 +0700

    #9 - Added User Create Form Domain

---
+++ src/main/java/com/hendisantika/example/domain/UserCreateForm.java
@@ -0,0 +1,73 @@
+package com.hendisantika.example.domain;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/28/17
+ * Time: 7:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class UserCreateForm {
+
+    @NotEmpty
+    private String email = "";
+
+    @NotEmpty
+    private String password = "";
+
+    @NotEmpty
+    private String passwordRepeated = "";
+
+    @NotNull
+    private Role role = Role.USER;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordRepeated() {
+        return passwordRepeated;
+    }
+
+    public void setPasswordRepeated(String passwordRepeated) {
+        this.passwordRepeated = passwordRepeated;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserCreateForm{" +
+                "email='" + email.replaceFirst("@.+", "@***") + '\'' +
+                ", password=***" + '\'' +
+                ", passwordRepeated=***" + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+}
