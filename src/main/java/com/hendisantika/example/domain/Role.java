6678137c71ab290e422f26b2be34dede0695fbdd
Hendi Santika <hendisantika@gmail.com>
2017-09-29 06:42:47 +0700

    #7 - Added Role Domain

---
+++ src/main/java/com/hendisantika/example/domain/Role.java
@@ -0,0 +1,18 @@
+package com.hendisantika.example.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/28/17
+ * Time: 7:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public enum Role {
+
+    USER, ADMIN
+
+}
