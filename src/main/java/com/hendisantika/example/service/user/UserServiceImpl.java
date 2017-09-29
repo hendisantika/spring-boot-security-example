556be92c51a049d9407246a6e84f13688bb4e8df
Hendi Santika <hendisantika@gmail.com>
2017-09-30 00:15:46 +0700

    #15 - Added UserServiceImpl.java

---
+++ src/main/java/com/hendisantika/example/service/user/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.hendisantika.example.service.user;
+
+import com.hendisantika.example.domain.User;
+import com.hendisantika.example.domain.UserCreateForm;
+import com.hendisantika.example.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/29/17
+ * Time: 7:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Optional<User> getUserById(long id) {
+        LOGGER.debug("Getting user={}", id);
+        return Optional.ofNullable(userRepository.findOne(id));
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        LOGGER.debug("Getting user by email={}", email.replaceFirst("@.*", "@***"));
+        return userRepository.findOneByEmail(email);
+    }
+
+    @Override
+    public Collection<User> getAllUsers() {
+        LOGGER.debug("Getting all users");
+        return userRepository.findAll(new Sort("email"));
+    }
+
+    @Override
+    public User create(UserCreateForm form) {
+        User user = new User();
+        user.setEmail(form.getEmail());
+        user.setPasswordHash(new BCryptPasswordEncoder().encode(form.getPassword()));
+        user.setRole(form.getRole());
+        return userRepository.save(user);
+    }
+
+}
