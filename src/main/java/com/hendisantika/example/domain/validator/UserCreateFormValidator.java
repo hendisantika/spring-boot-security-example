f2f3f5161db2229aa77a940463c74152706f9cae
Hendi Santika <hendisantika@gmail.com>
2017-10-01 06:36:31 +0700

    #16 - Added User Create Form Validator

---
+++ src/main/java/com/hendisantika/example/domain/validator/UserCreateFormValidator.java
@@ -0,0 +1,56 @@
+package com.hendisantika.example.domain.validator;
+
+import com.hendisantika.example.domain.UserCreateForm;
+import com.hendisantika.example.service.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/29/17
+ * Time: 9:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class UserCreateFormValidator implements Validator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserCreateFormValidator.class);
+    private final UserService userService;
+
+    @Autowired
+    public UserCreateFormValidator(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.equals(UserCreateForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        LOGGER.debug("Validating {}", target);
+        UserCreateForm form = (UserCreateForm) target;
+        validatePasswords(errors, form);
+        validateEmail(errors, form);
+    }
+
+    private void validatePasswords(Errors errors, UserCreateForm form) {
+        if (!form.getPassword().equals(form.getPasswordRepeated())) {
+            errors.reject("password.no_match", "Passwords do not match");
+        }
+    }
+
+    private void validateEmail(Errors errors, UserCreateForm form) {
+        if (userService.getUserByEmail(form.getEmail()).isPresent()) {
+            errors.reject("email.exists", "User with this email already exists");
+        }
+    }
+}
