0b509a741a078cb9c9a6d29f6a0e6bbd0475c68c
Hendi Santika <hendisantika@gmail.com>
2017-10-01 06:45:14 +0700

    #23 - Added Users Controller

---
+++ src/main/java/com/hendisantika/example/controller/UsersController.java
@@ -0,0 +1,38 @@
+package com.hendisantika.example.controller;
+
+import com.hendisantika.example.service.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/29/17
+ * Time: 10:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class UsersController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UsersController.class);
+    private final UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/users")
+    public ModelAndView getUsersPage() {
+        LOGGER.debug("Getting users page");
+        return new ModelAndView("users", "users", userService.getAllUsers());
+    }
+
+
+}
