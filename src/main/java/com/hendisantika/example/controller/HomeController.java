692ce66bd0d49319c11d8080164ba6a9a66787ba
Hendi Santika <hendisantika@gmail.com>
2017-10-01 06:39:27 +0700

    #20 - Added Home Controller

---
+++ src/main/java/com/hendisantika/example/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.hendisantika.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/29/17
+ * Time: 10:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class HomeController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        LOGGER.debug("Getting home page");
+        return "home";
+    }
+
+}
