09fd5738113dae8ab9131826677b3580dab307a1
Hendi Santika <hendisantika@gmail.com>
2017-10-01 06:44:44 +0700

    #21 - Added Login Controller

---
+++ src/main/java/com/hendisantika/example/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.hendisantika.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/29/17
+ * Time: 10:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class LoginController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView getLoginPage(@RequestParam Optional<String> error) {
+        LOGGER.debug("Getting login page, error={}", error);
+        return new ModelAndView("login", "error", error);
+    }
+
+}
