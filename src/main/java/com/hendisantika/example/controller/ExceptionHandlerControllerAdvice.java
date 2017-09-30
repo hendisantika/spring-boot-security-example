3b49cdc1cd1b93ea683375358fc09917ad829da0
Hendi Santika <hendisantika@gmail.com>
2017-10-01 06:38:54 +0700

    #18 - Added Exception Handler Controller Advice

---
+++ src/main/java/com/hendisantika/example/controller/ExceptionHandlerControllerAdvice.java
@@ -0,0 +1,32 @@
+package com.hendisantika.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.NoSuchElementException;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/29/17
+ * Time: 10:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@ControllerAdvice
+public class ExceptionHandlerControllerAdvice {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerControllerAdvice.class);
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNoSuchElementException(NoSuchElementException e) {
+        return e.getMessage();
+    }
+
+}
