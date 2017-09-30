a52ca00d26d399a4f565f6a610fffb521d5aacb9
Hendi Santika <hendisantika@gmail.com>
2017-10-01 06:38:20 +0700

    #18 - Added Current User Controller Advice

---
+++ src/main/java/com/hendisantika/example/controller/CurrentUserControllerAdvice.java
@@ -0,0 +1,30 @@
+package com.hendisantika.example.controller;
+
+import com.hendisantika.example.domain.CurrentUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/29/17
+ * Time: 10:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@ControllerAdvice
+public class CurrentUserControllerAdvice {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrentUserControllerAdvice.class);
+
+    @ModelAttribute("currentUser")
+    public CurrentUser getCurrentUser(Authentication authentication) {
+        return (authentication == null) ? null : (CurrentUser) authentication.getPrincipal();
+    }
+
+
+}
