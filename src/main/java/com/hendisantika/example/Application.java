d6aa3fbb2235e9773fdad0866a50ecfa5e8c5095
Hendi Santika <hendisantika@gmail.com>
2019-12-21 15:22:54 +0700

    #38 - Upgraded into Spring Boot 2.2.0.RELEASE version

--- src/main/java/com/hendisantika/example/Application.java
+++ src/main/java/com/hendisantika/example/Application.java
@@ -3,7 +3,7 @@ package com.hendisantika.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Created by IntelliJ IDEA.
